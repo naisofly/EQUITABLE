955a6eb05cfb9150b4c7270ddc16dba61db6a559
festiva1300 <3043378+festiva1300@users.noreply.github.com>
2024-03-24 14:11:51 +0900

    Change to a simple writing style

--- app/main.py
+++ app/main.py
@@ -3,15 +3,15 @@ import os
 import anthropic
 import streamlit as st
 
+st.title("Claude 3 by Streamlit")
+
 ai_model = os.environ.get("AI_MODEL")
 api_key = os.environ.get("API_KEY")
-
-st.title("Claude 3 by Streamlit")
 
 client = anthropic.Anthropic(
     api_key=api_key,
 )
-if "openai_model" not in st.session_state:
+if "ai_model" not in st.session_state:
     st.session_state["ai_model"] = ai_model
 
 if "messages" not in st.session_state:
@@ -29,26 +29,13 @@ if prompt := st.chat_input("What is up?"):
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
         full_response = ""
-        stream = client.messages.create(
+        with client.messages.stream(
             max_tokens=1024,
             messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
             model=st.session_state["ai_model"],
-            stream=True,
-        )
-        if stream:
-            for event in stream:
-                response = ""
-                match event.type:
-                    case "message_start":
-                        input_tokens = event.message.usage.input_tokens
-                    case "content_block_start":
-                        response = event.content_block.text
-                    case "content_block_delta":
-                        response = event.delta.text
-                    case "message_delta":
-                        output_tokens = event.usage.output_tokens
-
-                full_response += str(response) if response is not None else ""
+        ) as stream:
+            for text in stream.text_stream:
+                full_response += str(text) if text is not None else ""
                 message_placeholder.markdown(full_response + "▌")
         message_placeholder.markdown(full_response)
     st.session_state.messages.append({"role": "assistant", "content": full_response})
