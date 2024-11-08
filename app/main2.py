4066edf955c796c42fb7b419eaafc001b1472113
naisofly <naiyarah.h@gmail.com>
2024-11-08 14:55:50 +0400

    Resolve dependencies and add logo

--- app/main2.py
+++ app/main2.py
@@ -7,7 +7,7 @@ from elevenlabs.client import ElevenLabs
 import re
 
 # Path to the logo image
-logo_path = r"C:\Users\tejas\EQUITABLE\image\logo.png"
+logo_path = r"Logo.jpg"
 st.logo(logo_path,size="large")
 
 # Set up the Streamlit page title and caption
